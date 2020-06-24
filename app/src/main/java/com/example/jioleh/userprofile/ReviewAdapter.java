283f65a3240e7b26f3c9ae7b662dabe59f07a9ea
AlexCQY <59348559+AlexCQY@users.noreply.github.com>
2020-06-25 00:25:45 +0800

    style user profile and implement delete review

--- app/src/main/java/com/example/jioleh/userprofile/ReviewAdapter.java
+++ app/src/main/java/com/example/jioleh/userprofile/ReviewAdapter.java
@@ -1,8 +1,11 @@
 package com.example.jioleh.userprofile;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
-import android.media.Image;
+import android.content.DialogInterface;
 import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,18 +13,18 @@ import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
+
 import androidx.annotation.NonNull;
-import androidx.annotation.UiThread;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.jioleh.R;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
-import java.util.Locale;
+
 
 public class ReviewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
@@ -31,14 +34,26 @@ public class ReviewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
 
     private List<Review> lst;
     private float avg;
+    private String uid;
+    private String viewer_uid;
+    private ViewGroup viewGroup;
 
     public ReviewAdapter(){
         this.lst = new ArrayList<>();
     }
 
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public void setViewer_uid(String viewer_uid) {
+        this.viewer_uid = viewer_uid;
+    }
+
     @NonNull
     @Override
     public  RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        viewGroup = parent;
         if (viewType == TYPE_ITEM) {
             View row = LayoutInflater.from(parent.getContext()).inflate(R.layout.review_row, parent, false);
             return new ReviewHolder(row);
@@ -74,7 +89,38 @@ public class ReviewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
             rv.review_words.setText(review.getWordsOfReview());
             rv.reviewer_username.setText(review.getFrom_username());
             rv.date.setText(timeStampToString(review.getTimeOfPost()));
-            rv.review_rating.setText(String.format("%.1f", review.getRating()));
+            rv.review_rating.setText(String.format("%.1f", (review.getRating())));
+
+            if (review.getFrom_uid().equals(this.viewer_uid)) {
+                rv.delete_review.setVisibility(View.VISIBLE);
+                rv.delete_review.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        new AlertDialog.Builder(viewGroup.getContext())
+                                .setTitle("Delete Review")
+                                .setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        FirebaseFirestore.getInstance()
+                                                .collection("users")
+                                                .document(uid)
+                                                .collection("Reviews")
+                                                .document(review.getDocumentId())
+                                                .delete();
+                                        lst.remove(holder.getAdapterPosition());
+                                        notifyItemRemoved(holder.getAdapterPosition());
+                                        notifyItemRangeChanged(holder.getAdapterPosition(),lst.size());
+
+                                    }
+                                })
+                                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                    }
+                                }).show();
+                    }
+                });
+            }
         }
 
     }
@@ -117,7 +163,7 @@ public class ReviewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
     static class ReviewHolder extends RecyclerView.ViewHolder{
 
         ImageView iv_reviewer_profilePic;
-        TextView review_words, reviewer_username, date,review_rating;
+        TextView review_words, reviewer_username, date,review_rating,delete_review;
 
         ReviewHolder(@NonNull View itemView) {
             super(itemView);
@@ -126,6 +172,7 @@ public class ReviewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
             reviewer_username = itemView.findViewById(R.id.review_row_username);
             date = itemView.findViewById(R.id.review_row_date);
             review_rating = itemView.findViewById(R.id.review_row_rating);
+            delete_review = itemView.findViewById(R.id.review_delete);
         }
 
 
